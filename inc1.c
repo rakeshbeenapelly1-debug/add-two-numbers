05103d0c6144e3ce8c405f43fa3bd178edada312
rakesh <rakeshbeenapelly1@gmail.com>
2026-06-16 11:05:56 +0530

    first commit

---
+++ inc1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2, sum;
+
+    // Prompt the user to enter two integers
+    printf("Enter two integers: ");
+
+    // Read the inputs and store them in variables
+    scanf("%d %d", &num1, &num2);
+
+    // Calculate the sum using the + operator
+    sum = num1 + num2;
+
+    // Display the final result
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
